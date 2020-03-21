46005ccaf5b9be1b1257e298412be81d9c21422b
Bill Wilke <billwilke2@gmail.com>
2020-03-20 18:32:37 -0600

    Removed global variable

--- src/part.js
+++ src/part.js
@@ -1,18 +1,11 @@
-var validTypes = [
-  'shell',
-  'hyperdrive',
-  'computer',
-  'life support',
-  'landing gear',
-  undefined
-]
 
 class Part {
   constructor(object) {
-    this.name = object.name ;
-    this.type = validTypes.includes(object.type) ? object.type : undefined;
+    this.name = object.name;
     this.value = object.value;
     this.broken = false;
+    this.validTypes = ['landing gear', 'shell', 'hyperdrive', 'computer', 'life support', 'landing gear', undefined]
+    this.type = this.validTypes.includes(object.type) ? object.type : undefined;
   }
 
   isValid() {
